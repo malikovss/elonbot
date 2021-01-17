af1369b7bff780fbc157e9dfc90fc49919ef31b4
Suhrob Malikov <oopanndaa@gmail.com>
2021-01-17 14:34:13 +0500

    ssl cert removed

--- bot/app.py
+++ bot/app.py
@@ -1,19 +1,18 @@
 from aiogram.utils.executor import start_webhook
-from loader import bot, SSL_CERTIFICATE, ssl_context
+from loader import bot
 from config import WEBAPP_HOST, WEBAPP_PORT, WEBHOOK_PATH, WEBHOOK_URL
 
 
 async def on_startup(dp):
     await bot.set_webhook(
         url=WEBHOOK_URL,
-        certificate=SSL_CERTIFICATE
     )
 
 
 
 if __name__ == '__main__':
     from handlers import dp
-    
+
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
@@ -21,5 +20,4 @@ if __name__ == '__main__':
         skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
-        ssl_context=ssl_context
     )
--- bot/config.py
+++ bot/config.py
@@ -15,6 +15,3 @@ WEBHOOK_URL = f"{WEBHOOK_HOST}:{WEBHOOK_PORT}{WEBHOOK_PATH}"
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'  # or ip
 WEBAPP_PORT = int(os.getenv('PORT'))
-
-WEBHOOK_SSL_CERT = "webhook_cert.pem"
-WEBHOOK_SSL_PRIV = "webhook_pkey.pem"
