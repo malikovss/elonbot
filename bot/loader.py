c20dd18b1a16cc84f25e99ae2397d7a9a707aa0f
Suhrob Malikov <oopanndaa@gmail.com>
2021-01-17 14:35:58 +0500

    bug fixed

--- bot/loader.py
+++ bot/loader.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from config import TOKEN, WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV
+from config import TOKEN
 
 bot = Bot(TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
